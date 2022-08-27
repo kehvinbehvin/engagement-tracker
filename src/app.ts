1de4117cb2fedb0a6f832e7398c4e651c687f598
kevin <hokevin78@gmail.com>
2022-08-28 02:54:32 +0800

    feat(app): add Hello world test

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express from "express"
+import express, {Request, Response} from 'express';
+
 require("dotenv").config();
 
 const app = express();
 
 const { PORT } = process.env
 
+app.get('/', (req: Request, res: Response) => {
+    return res.send('Hello World!')
+  })
+
 app.listen(PORT, () => {
     console.log("info",`Application listening at port ${PORT}`)
 })
+
