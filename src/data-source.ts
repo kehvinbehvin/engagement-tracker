5236b7adec1a335333e96ebb76ef3f433486d6c1
kevin <hokevin78@gmail.com>
2022-08-29 00:45:06 +0800

    build(DataSource): add database connection method for production/staging

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,19 +9,39 @@ const {
     DB_PORT_POSTGRES, 
     DB_DATABASE_POSTGRES, 
     DB_USERNAME_POSTGRES, 
-    DB_PASSWORD_POSTGRES 
+    DB_PASSWORD_POSTGRES,
+    NODE_ENV,
+    DATABASE_URL
     } = process.env
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: DB_HOST_POSTGRES,
-    port: Number(DB_PORT_POSTGRES),
-    username: DB_USERNAME_POSTGRES,
-    password: DB_PASSWORD_POSTGRES,
-    database: DB_DATABASE_POSTGRES,
-    synchronize: true,
-    logging: true,
-    entities: [User],
-    subscribers: [],
-    migrations: [],
-})
+function getDataSource() {
+    if (NODE_ENV === "development") {
+        return new DataSource({
+            type: "postgres",
+            host: DB_HOST_POSTGRES,
+            port: Number(DB_PORT_POSTGRES),
+            username: DB_USERNAME_POSTGRES,
+            password: DB_PASSWORD_POSTGRES,
+            database: DB_DATABASE_POSTGRES,
+            synchronize: true,
+            logging: true,
+            entities: [User],
+            subscribers: [],
+            migrations: [],
+        })
+    } else {
+        return new DataSource({
+            type: "postgres",
+            url: DATABASE_URL,
+            synchronize: true,
+            logging: true,
+            entities: [User],
+            migrations: [],
+            subscribers: [],
+        })
+    }
+}
+
+const AppDataSource = getDataSource()
+
+export { AppDataSource }
