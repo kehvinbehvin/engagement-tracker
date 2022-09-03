d0ea5547bb6f0e298df1805f395a4f32c9168706
kevin <hokevin78@gmail.com>
2022-09-03 11:54:54 +0800

    debug: disable cors

--- src/app.ts
+++ src/app.ts
@@ -14,19 +14,19 @@ import userRoutes from "./user/user.routes"
 // Middleware
 import errorHandler from "./utils/error_handling/errorHandler.middleware"
 import routeLogger from "./utils/logger/src/routeLogger.middleware";
-var cors = require('cors')
+// var cors = require('cors')
 
 
 const app: Express = express();
 const port = process.env.PORT;
-const nginxHost = process.env.NGINX_HOST;
+// const nginxHost = process.env.NGINX_HOST;
 
-var corsOptions = {
-    origin: nginxHost,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+// var corsOptions = {
+//     origin: nginxHost,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(routeLogger);
 app.use(errorHandler);
@@ -40,5 +40,5 @@ AppDataSource.initialize().then(async () => {
 
 app.listen(port, () => {
     logger.log("info",`Server is running on port ${port}`);
-    logger.log("info",`Server listening to nginx at ${nginxHost}`)
+    // logger.log("info",`Server listening to nginx at ${nginxHost}`)
 });
