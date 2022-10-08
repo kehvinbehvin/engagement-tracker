011fd84d589277e08737021a774b2ca3bf4db775
kevin <hokevin78@gmail.com>
2022-10-08 13:57:55 +0800

    feat(activity): add create activity controller method

--- src/activity/activity.controller.ts
+++ src/activity/activity.controller.ts
@@ -5,6 +5,7 @@ import { HTTPBadRequestError } from "../utils/error_handling/src/HTTPBadRequestE
 import { HTTPAccessDeniedError } from "../utils/error_handling/src/HTTPAccessDeniedError"
 import { HTTPNotFoundError } from "../utils/error_handling/src/HTTPNotFoundError"
 import httpStatusCodes from "../utils/error_handling/configs/httpStatusCodes"
+import { createNewActivityTask } from "./activity.manager"
 
 export async function getActivity() {
 
@@ -18,6 +19,32 @@ export async function patchActivity() {
     
 }
 
-export async function createActivity() {
-    
+export async function createActivity(req: Request, res: Response, next: NextFunction) {
+    try {
+        const data = req.body;
+
+        const keyFields = ["activityDate", "type", "newcomerId", "adminIds"];
+        
+        activityLogger.log("debug", {
+            "message": req.body
+        })
+
+        if (!completeKeys(keyFields,data)) {
+            return next(new HTTPBadRequestError("Incomplete data"));
+        }
+
+        const newActivity = await createNewActivityTask(data);
+
+        activityLogger.log("info",`${data.email} created`);
+
+        const response = {
+            "Message": "Activity added",
+            "Activity id": `${newActivity.id}`,
+        }
+
+        return res.json(response).status(httpStatusCodes.OK);
+    } catch (error: any) {
+        activityLogger.log("error", error);
+        return next(error);
+    }
 }
